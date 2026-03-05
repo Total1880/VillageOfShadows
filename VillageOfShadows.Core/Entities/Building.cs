9b1b887bfb7b55166133e4ba6ec9b2b642276ea4
Olav Hendrickx <olhendri@Q8INT.COM>
2026-03-05 13:58:30 +0100

    Add Stockpile

---
+++ VillageOfShadows.Core/Entities/Building.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VillageOfShadows.Core.Entities
+{
+    public abstract class Building : Entity
+    {
+        public float InventorySize { get; set; } = 0;
+        public float MaxInventory { get; init; } = 0;
+        public required IList<Entity> Inventory { get; set; }
+    }
+}
---
+++ VillageOfShadows.Core/Entities/StockPile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VillageOfShadows.Core.Entities
+{
+    public class StockPile : Building
+    {
+        public StockPile()
+        {
+            MaxInventory = 100;
+        }
+        public override Entity Create()
+        {
+            return new StockPile
+            {
+                Inventory = []
+            };
+        }
+    }
+}
